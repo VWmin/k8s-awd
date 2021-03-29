4bdf7b8b368ca06d1ac16540cd658de0ee3c4a81
VWmin <fawsuki@gmail.com>
2021-03-29 11:08:23 +0800

    first commit

---
+++ src/main/java/com/vwmin/k8sawd/K8sAwdApplication.java
@@ -0,0 +1,13 @@
+package com.vwmin.k8sawd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8sAwdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(K8sAwdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vwmin/k8sawd/K8sAwdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vwmin.k8sawd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8sAwdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
