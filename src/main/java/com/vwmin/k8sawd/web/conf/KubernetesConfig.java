0e0444c63e8685dc5d48de9554e719e068544356
VWmin <fawsuki@gmail.com>
2021-03-31 12:58:16 +0800

    创建namespace

--- src/main/java/com/vwmin/k8sawd/web/conf/KubernetesConfig.java
+++ src/main/java/com/vwmin/k8sawd/web/conf/KubernetesConfig.java
@@ -1,15 +1,19 @@
 package com.vwmin.k8sawd.web.conf;
 
-import io.fabric8.kubernetes.client.Config;
-import io.fabric8.kubernetes.client.ConfigBuilder;
-import io.fabric8.kubernetes.client.DefaultKubernetesClient;
-import io.fabric8.kubernetes.client.KubernetesClient;
+import cn.hutool.core.util.StrUtil;
+import io.fabric8.kubernetes.api.model.Namespace;
+import io.fabric8.kubernetes.api.model.NamespaceBuilder;
+import io.fabric8.kubernetes.api.model.NamespaceList;
+import io.fabric8.kubernetes.client.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+import java.util.stream.Stream;
 
 /**
  * @author vwmin
@@ -22,13 +26,22 @@ import org.springframework.context.annotation.Configuration;
 @EnableConfigurationProperties(KubernetesConfig.class)
 @ConfigurationProperties(prefix = "k8s-awd")
 public class KubernetesConfig {
+
+    private static final String DEFAULT_NAMESPACE = "awd";
+
     private String masterUrl;
     private String caCertFile;
     private String clientCertFile;
     private String clientKeyFile;
+    private String namespace;
 
     @Bean
     public KubernetesClient kubernetesClient() {
+
+        if (StrUtil.isEmpty(namespace)){
+            namespace = DEFAULT_NAMESPACE;
+        }
+
         Config config = new ConfigBuilder()
                 .withMasterUrl(masterUrl)
                 .withCaCertFile(caCertFile)
@@ -36,6 +49,20 @@ public class KubernetesConfig {
                 .withClientKeyFile(clientKeyFile)
                 .build();
         config.setTrustCerts(true);
-        return new DefaultKubernetesClient(config);
+        DefaultKubernetesClient client = new DefaultKubernetesClient(config);
+
+
+        // 检查命名空间
+        if (client.namespaces().list().getItems().stream().noneMatch(e -> namespace.equals(e.getMetadata().getName()))) {
+            client.namespaces().create(new NamespaceBuilder()
+                    .withNewMetadata()
+                    .withName(namespace)
+//                    .addToLabels("a", "label")
+                    .endMetadata()
+                    .build());
+        }
+
+
+        return client.inNamespace(namespace);
     }
 }
